078d376ec5e5ce21ac9be56505c5c9c004014ca7
admin <admin@DESKTOP-13MN080>
2023-07-27 18:12:45 +0900

    20230727

---
+++ miniproject/src/main/java/co/micol/miniproject/App.java
@@ -0,0 +1,13 @@
+package co.micol.miniproject;
+
+import co.micol.miniproject.menu.MainMenu;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	MainMenu menu = new MainMenu();
+    	menu.run();
+    }
+}
+
